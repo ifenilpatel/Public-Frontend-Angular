9663c6f64f6dd664acf0cdd38be56d431e78e916
Fenil patel <fens98@gmail.com>
2024-08-04 12:29:50 +0530

    update dashboard with admin page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,6 +6,10 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Public Frontend Angular ' + environment.version;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
